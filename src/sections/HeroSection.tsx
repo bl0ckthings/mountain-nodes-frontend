0f593ec4f8335186a8148f771fa0d06e8fce5e79
Marvin Cano <30679572+BeezBeez@users.noreply.github.com>
2022-05-11 15:16:30 +0200

    Oui keyframes bref...

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -133,6 +133,13 @@ const ButtonContainer = styled.div`
     }
 `
 
+const shake = keyframes`
+    0%{transform:rotateZ(0)}
+    33%{transform:rotateZ(10deg)}
+    66%{transform:rotateZ(-10deg)}
+    100%{transform:rotateZ(0deg)}
+`
+
 const Social = styled.div`
     display: flex;
     place-content: center;
@@ -143,15 +150,8 @@ const Social = styled.div`
     transition: .1s ease-in-out;
     cursor: pointer;
 
-    @keyframes shake {
-        0%{transform:rotateZ(0)}
-        33%{transform:rotateZ(10deg)}
-        66%{transform:rotateZ(-10deg)}
-        100%{transform:rotateZ(0deg)}
-    }
-
     &:hover {
-        animation: shake 0.7s ;
+        animation: ${shake} 0.7s;
         background-color: rgba(255,255,255,0.5);
     }
 
